55fc4cbda09e7a6b507cbe829e4d59f65fd97fe0
Anne Mennen <amennen@princeton.edu>
2017-01-22 12:58:19 -0500

    s72

--- getratings_rt.m
+++ getratings_rt.m
@@ -10,7 +10,7 @@ projectName = 'motStudy03';
 base_path = [fileparts(which('mot_realtime02.m')) filesep];
 
 % don't put in 22 until have subject
-svec = [3 4 5];
+svec = [3 4 5 6];
 
 NSUB = length(svec);
 recallSession = [20 24];
